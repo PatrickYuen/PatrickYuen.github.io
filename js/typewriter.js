b131fc70a9357f857e69b90cad66a1335f0f3a61
PatrickYuen <ply209@nyu.edu>
2016-12-26 19:55:24 -0500

    Added waiting

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,22 +1,31 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [ "Patrick Yuen" ];
+  var dataText = "Patrick Yuen";
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
   function typeWriter(text, i) {
     // chekc if text isn't finished yet
     if (i < (text.length)) {
-      // add next character to h1
-     document.getElementById("title").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+		// add next character to h1
+		if(i >= 0) {
+			document.getElementById("title").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
 
-      // wait for a while and call this function again for next character
-      setTimeout(function() {
-        typeWriter(text, i + 1)
-      }, 100);
+			//Wait for a while and call this function again for next character
+			setTimeout(function() {
+			typeWriter(text, i + 1)
+			}, 150);
+		} else {
+			document.getElementById("title").innerHTML = '<span aria-hidden="true"></span>';
+			
+			//Wait a little bit
+			setTimeout(function() {
+				typeWriter(text, i + 1)
+			}, 1500);
+		}	
     }
   }
   
   // start the text animation
-  typeWriter(dataText[0], 0);
+  typeWriter(dataText, -1);
 });
